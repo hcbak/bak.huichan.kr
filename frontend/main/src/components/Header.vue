0f60418ef56d43c4d618752bf88004f9d07db45f
Huichan Bak <hc.bak@outlook.com>
2025-03-26 11:26:31 +0900

    feat: axios 의존성 추가 및 Navbar 백엔드 연동

--- frontend/main/src/components/Header.vue
+++ frontend/main/src/components/Header.vue
@@ -1,66 +1,59 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useToast } from '../utils/toast'
+import axios from 'axios'
 
 const { showInfoToast } = useToast();
 const router = useRouter();
 
-const items = ref([
-  {
-    label: 'Home',
-    route: '/'
-  },
-  {
-    label: 'About',
-    route: '/about'
-  },
-  {
-    label: 'Projects',
-    items: [
-      {
-        label: 'Components'
-      },
-      {
-        label: 'Blocks'
-      },
-      {
-        label: 'UI Kit'
-      }
-    ]
-  },
-  {
-    label: 'Contact'
-  }
-]);
+const items = ref(null);
 
+const fetchMenuItem = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_SERVER_URI}/api/v1/menu`);
+    items.value = response.data;
+  } catch (error) {
+    console.error(error);
+    if (!items.value) {
+      items.value = {
+        name: 'Home',
+        url: '/'
+      };
+    }
+  }
+};
 
 const handleSubItemClick = (subItem) => {
-  if (subItem.route) {
-    router.push(subItem.route);
+  if (subItem.url) {
+    router.push(subItem.url);
   } else {
     showInfoToast('준비 중인 기능입니다.');
   }
 };
 
+onMounted( () => {
+  fetchMenuItem();
+});
+
 </script>
 
 <template>
   <BNavbar toggleable="md">
-    <template v-if="items[0]">
-      <BNavbarBrand :to="items[0].route">{{ items[0].label }}</BNavbarBrand>
+    <template v-if="items">
+      <BNavbarBrand :to="items.url">{{ items.name }}</BNavbarBrand>
       <BNavbarToggle target="nav-collapse" />
-      <BCollapse v-if="items.length > 1" id="nav-collapse" is-nav>
+      <BCollapse v-if="items.sub" id="nav-collapse" is-nav>
         <BNavbarNav>
-          <template v-for="item in items.slice(1)" :key="item">
-            <BNavItem v-if="item.route" :to="item.route">{{ item.label }}</BNavItem>
-            <BNavItemDropdown v-if="item.items" :text="item.label">
+          <template v-for="item in items.sub" :key="item">
+            <BNavItem v-if="!item.sub" @click="handleSubItemClick(item)">{{ item.name }}</BNavItem>
+            <BNavItemDropdown v-if="item.sub" :text="item.name">
               <BDropdownItem
-                v-for="subItem in item.items"
+                v-for="subItem in item.sub"
                 :key="subItem"
                 @click="handleSubItemClick(subItem)"
               >
-                {{ subItem.label }}
+                {{ subItem.name }}
               </BDropdownItem>
             </BNavItemDropdown>
           </template>
